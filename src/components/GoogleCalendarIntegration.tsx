eee157ceb73d602a22b2909b6cc5f034dbcef08d
Grayson Leversha <grayson@greenstone.software>
2024-08-14 10:32:04 +1200

    Fixed Google Integration and added API key

--- src/components/GoogleCalendarIntegration.tsx
+++ src/components/GoogleCalendarIntegration.tsx
@@ -1,21 +1,39 @@
 import { useGoogleLogin } from '@react-oauth/google';
 import { gapi } from 'gapi-script';
 
+interface CalendarEvent {
+  id: string;
+  summary: string;
+  start: {
+    dateTime: string;
+    timeZone: string;
+  };
+  end: {
+    dateTime: string;
+    timeZone: string;
+  };
+}
+
+interface CalendarResponse {
+  items: CalendarEvent[];
+}
+
 const GoogleCalendarIntegration = () => {
   const login = useGoogleLogin({
     onSuccess: async (tokenResponse) => {
       const accessToken = tokenResponse.access_token;
-      gapi.client.setApiKey('YOUR_GOOGLE_API_KEY');
+      gapi.client.setApiKey('AIzaSyDRibDKlvClyl0aR4_N6WlrORuT-XkUk08');
       gapi.client.load('calendar', 'v3', () => {
         const request = gapi.client.calendar.events.list({
-          'calendarId': 'primary',
-          'timeMin': (new Date()).toISOString(),
-          'showDeleted': false,
-          'singleEvents': true,
-          'maxResults': 10,
-          'orderBy': 'startTime',
+          calendarId: 'primary',
+          timeMin: (new Date()).toISOString(),
+          showDeleted: false,
+          singleEvents: true,
+          maxResults: 10,
+          orderBy: 'startTime',
         });
-        request.execute((response) => {
+
+        request.execute((response: CalendarResponse) => {
           const events = response.items;
           console.log(events);
         });
@@ -28,3 +46,5 @@ const GoogleCalendarIntegration = () => {
     <button onClick={() => login()}>Connect Google Calendar</button>
   );
 };
+
+export default GoogleCalendarIntegration;
